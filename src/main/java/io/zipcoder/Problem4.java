3d66bff3263abe9c69cc85be799e5f84bd0fcc3a
graham <grahamnilsen@gmail.com>
2017-11-27 16:09:20 -0500

    finished

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,46 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    public String isPalindrome(String input) {
+        char[] charArrayofString = input.toCharArray();
+
+        if (checkPossiblePalindrome(createAnagramPalindromeMap(charArrayofString))) {
+            return "YES";
+        } else {
+            return "NO";
+        }
+    }
+
+    public HashMap createAnagramPalindromeMap(char[] charArray) {
+        HashMap<Character, Integer> anagramMap = new HashMap<Character, Integer>();
+        for (char letter : charArray) {
+            if (anagramMap.containsKey(letter)) {
+                Integer newItemCount = anagramMap.get(letter);
+                newItemCount++;
+                anagramMap.put(letter, newItemCount);
+            } else {
+                anagramMap.put(letter, 1);
+            }
+        }
+        return anagramMap;
+    }
+
+    public boolean checkPossiblePalindrome(HashMap<Character, Integer> anagramMap) {
+        int oddCount = 0;
+        for (Map.Entry<Character, Integer> letter : anagramMap.entrySet()) {
+            if (letter.getValue() % 2 != 0) {
+                oddCount++;
+            }
+        }
+        if (oddCount > 1) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,47 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class Problem4Test {
+
+    Problem4 problem4 = new Problem4();
+    char[] actualArray = {'A','B','B','C','C','C'};
+
+    @Test
+    public void isPalindromeTest(){
+        String expected = "YES";
+        String actual = problem4.isPalindrome("cdcdcdcdeeeef");
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void createAnagramPalindromeMap(){
+        HashMap<Character, Integer> expected = new HashMap<Character, Integer>();
+        expected.put('A',1);
+        expected.put('B',2);
+        expected.put('C', 3);
+
+        char[] actualArray = {'A','B','B','C','C','C'};
+        HashMap<Character,Integer> actual = problem4.createAnagramPalindromeMap(actualArray);
+
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void checkPossiblePalindrome(){
+        HashMap<Character, Integer> expectedMap = new HashMap<Character, Integer>();
+        expectedMap.put('A',1);
+        expectedMap.put('B',2);
+        expectedMap.put('C', 4);
+
+        boolean expected = true;
+        boolean actual = problem4.checkPossiblePalindrome(expectedMap);
+
+        Assert.assertEquals(expected,actual);
+    }
 }
